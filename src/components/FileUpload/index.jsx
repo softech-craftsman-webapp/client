35c95c9e6f27e5ed44be0719c9f98cae60784c1a
Li-Xilun <90523665+Li-Xilun@users.noreply.github.com>
2021-10-27 13:06:50 +0200

    Fulfill FileUpload and FileList components
    Two components for user upload their files or images, can tell them upload successful or not, and list their files' information.

--- src/components/FileUpload/index.jsx
+++ src/components/FileUpload/index.jsx
@@ -1,5 +1,5 @@
 import React ,{useState} from 'react';
-import { useHistory, Link } from "react-router-dom";
+import { useHistory} from "react-router-dom";
 import fetcher from '../../helpers/fetcher';
 import Button from '../Button';
 import Input from '../Input';
@@ -39,16 +39,29 @@ function FileUpload()
     
     return(
       <div>
-        <form onSubmit={this.submit}>
-            <input type="file" name='file'/>
-            {<Button 
-            type= "submit" 
-            >
+       
+            <div>
+            <Button 
+            type= "submit" onClick={submit}>
             Upload </Button>
-            }
-        </form>
-        <div>
+            </div>
             
+       
+        <div>
+        <Input type="file" name='file' accept="image/jpeg, image/jpg, image/png, image/gif, image/bmp, application/pdf, video/x-flv, video/mp4, application/x-mpegURL,
+               video/MP2T video/3gpp, video/quicktime, video/x-msvideo, video/x-ms-wmv"
+               onChange={changeHandler}/>
+        {
+          selectedFile ? (
+            <div className="pt-3 pb-3">
+              <p>Filename: {selectedFile.name}</p>
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
+            </div>
+          ) 
+          : 
+            null
+        }
         </div>
       </div>
 
