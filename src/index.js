962eaa8aeee80497898f2a1d87e7e40e02bbc36b
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-10-29 22:17:12 +0200

    JSX added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { Toaster } from 'react-hot-toast';
 // styles
 import './global.scss';
 // Please comment tailwindcss in production
-import './tailwind.css'
-// import './index.css'
+// import './tailwind.css'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
