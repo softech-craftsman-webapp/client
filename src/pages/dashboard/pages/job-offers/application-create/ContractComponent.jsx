259ecd0b32f49f6a408af2ae93ce7ac1a944bab7
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-11-07 17:31:15 +0100

    Updated user story

---
+++ src/pages/dashboard/pages/job-offers/application-create/ContractComponent.jsx
@@ -0,0 +1,53 @@
+function ContractComponent() {
+    return (
+        <div className="text-sm text-gray-600 text-left px-1">
+            <p>Updated on Nov 1, 2021</p>
+            <p className="py-1 text-xs">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur ornare mi eget cursus. 
+                Aliquam consequat eleifend magna ultrices semper. Proin malesuada, augue ac dapibus volutpat, urna 
+                neque sollicitudin risus, a commodo mi libero id diam. Cras vehicula venenatis lectus, non posuere 
+                magna bibendum vel. Aliquam viverra, magna at sagittis vehicula, justo purus ultricies diam, vel 
+                tempus nibh turpis vitae massa. Nulla facilisi. Morbi facilisis ornare orci, mollis mollis justo 
+                posuere in. Integer interdum lorem augue, sed imperdiet erat ultricies non. Interdum et malesuada 
+                fames ac ante ipsum primis in faucibus. Donec volutpat, justo id tempor posuere, dolor mauris varius 
+                lacus, nec mollis augue turpis ac nisl. Praesent vulputate est elementum purus cursus, eget sagittis 
+                massa sollicitudin. Donec ornare ac arcu ac gravida.
+            </p>
+            <p className="py-1 text-xs">
+                Nulla consequat diam dolor, ac pharetra dolor maximus malesuada. Aliquam molestie sem vel felis maximus 
+                facilisis. Donec bibendum et orci nec tempus. In venenatis nibh nisl, accumsan aliquam diam molestie id. 
+                Nulla risus orci, vehicula eu molestie vitae, pellentesque ac magna. Praesent dignissim lectus id tincidunt 
+                semper. Mauris lacinia vel mauris et rhoncus. Cras congue, elit at aliquet tristique, magna diam lobortis 
+                nulla, eu molestie tortor tellus interdum mi.
+            </p>
+            <p className="py-1 text-xs">
+                Donec et purus in lorem rhoncus interdum eget non mi. Nullam suscipit nisl aliquam eleifend tristique. 
+                Donec in finibus velit, eu congue lacus. Class aptent taciti sociosqu ad litora torquent per conubia 
+                nostra, per inceptos himenaeos. Ut efficitur lobortis semper. Integer interdum ultrices mauris vel luctus. 
+                Nam in ultrices diam, eu lobortis arcu. Nullam ultricies mauris ac ex porta, ut iaculis urna sollicitudin. 
+                Nunc egestas accumsan pharetra. Pellentesque dapibus mi vel odio finibus, a accumsan nulla iaculis. Fusce 
+                ullamcorper fringilla elit, in luctus libero tincidunt quis. Nulla tempor eros semper erat pulvinar, ac 
+                tempus mauris hendrerit. Sed tristique dui purus, eu pharetra augue suscipit ut.
+            </p>
+            <p className="py-1 text-xs">
+                Morbi vitae elit lectus. Mauris non luctus lacus, ut lacinia sapien. Suspendisse congue vitae nisl in molestie. 
+                Phasellus vel tellus in nisl aliquam aliquam. Donec viverra eu quam ac imperdiet. In sed tempor risus. 
+                Aliquam erat volutpat. Pellentesque placerat tortor ut odio pulvinar, et sodales mi vulputate. Sed posuere, 
+                leo non imperdiet congue, diam erat vulputate nunc, quis eleifend elit neque vel nisi. Nulla sed eros 
+                consectetur, egestas felis a, condimentum purus. Mauris ac libero commodo mi gravida egestas id vitae 
+                turpis. Etiam in tortor in urna gravida porttitor ac tempor nulla. Maecenas fermentum purus ut arcu molestie, 
+                a dapibus leo feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis 
+                egestas. Vivamus vel elit et diam iaculis vestibulum. Cras eget felis volutpat elit convallis suscipit.
+            </p>
+            <p className="py-1 text-xs">
+                Sed sem metus, dictum tempor laoreet ac, congue id dolor. Duis varius lacus velit, quis eleifend diam 
+                scelerisque ut. Fusce in sem nec augue cursus dapibus at sed mi. Sed ac erat mattis, auctor justo in, 
+                tempor nulla. Etiam id metus sed metus dignissim eleifend. Integer condimentum elit nec imperdiet euismod. 
+                Duis id auctor neque. Proin placerat, ipsum vel viverra dignissim, tortor nisl finibus nisi, quis tincidunt 
+                felis orci ut nisi. Vestibulum vitae scelerisque nulla. Sed et venenatis orci, et elementum ipsum.
+            </p>
+        </div>
+    );
+}
+
+export default ContractComponent;
