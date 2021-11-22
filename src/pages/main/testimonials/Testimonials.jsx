6765155e0a72d0ab3b9355cb120d353342e10182
Zsanett Armbruszt <sz2jjs@inf.elte.hu>
2021-11-22 21:25:17 +0100

    Nikitas amazing quote

--- src/pages/main/testimonials/Testimonials.jsx
+++ src/pages/main/testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import wedding from '../../../assets/wedding.jpg'
+import nikita from '../../../assets/nikita.jpeg'
 
 function Testimonials() {
   return (
@@ -23,14 +23,16 @@ function Testimonials() {
                   <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                     <path d="M37.89 58.338c-2.648-5.63-3.572-10.045-2.774-13.249.8-3.203 8.711-13.383 23.737-30.538l2.135.532c-6.552 10.033-10.532 17.87-11.939 23.515-.583 2.34.22 6.158 2.41 11.457l-13.57 8.283zm-26.963-6.56c-2.648-5.63-3.572-10.046-2.773-13.25.799-3.203 8.71-13.382 23.736-30.538l2.136.533c-6.552 10.032-10.532 17.87-11.94 23.515-.583 2.339.22 6.158 2.41 11.456l-13.57 8.283z" />
                   </svg>
-                  <img className="relative rounded-full" src={wedding} width="96" height="96" alt="Testimonial 01" />
+                  <img className="relative rounded-full" src={nikita} width="96" height="96" alt="Testimonial 01" />
                 </div>
-                <blockquote className="text-xl font-medium mb-4">
-                  “ I love this product and would recommend it to anyone. Could be not easier to use, and our multiple websites are wonderful. We get nice comments all the time. “
+                <blockquote className="text-l font-small mb-4">
+                "<i>The sight of the abyss should give rise to thoughts not <br/> about the abyss, but about the bridge - Mikhail Weller.</i>
+                <br/><br/> 
+                 And we built such a bridge named Hiringo, which has no analogues in Hungary. You must check these words yourself, and make sure that a better product than ours - you will not find!"
                 </blockquote>
-                <cite className="block font-bold text-lg not-italic mb-1">Darya Finger</cite>
+                <cite className="block font-bold text-lg not-italic mb-1">Nikita Letiushev</cite>
                 <div className="text-gray-600">
-                  <span>CEO & Co-Founder</span> 
+                  <span>Co-Founder</span> 
                 </div>
               </div>
 
