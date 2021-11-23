d17b542423a89a24e2c5fe67c6907b65a54a34f8
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-11-23 12:44:28 +0100

    Tailwind css for helping purposes removed

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import GeoTracker from './components/Location/GeoTracker';
 
 // styles
 import './global.scss';
-// Please comment tailwindcss in production
-import './tailwind.css'
-// import './index.css'
+import './index.css'
 
 let geoDetails = {};
 let latitude = 1;
