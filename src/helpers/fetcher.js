b5e12e0c576b4b279183e56db15cb74ada26eb56
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-11-29 16:40:36 +0100

    axios fix

--- src/helpers/fetcher.js
+++ src/helpers/fetcher.js
@@ -11,50 +11,64 @@ const axiosInstance = axios.create({
   timeout: 30 * 1000,
   headers: {
     'X-Request-ID': uid,
-    'Accept': 'application/json'
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
   },
   responseType: 'json'
 });
 
-const refreshAuthLogic = failedRequest =>
-    axios.get(`${process.env.REACT_APP_API_BASE}/auth/refresh`, {
-      crossdomain: true,
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('refresh_token')}`,
-        'X-Request-ID': uid,
-        'Accept': 'application/json'
-      }
-    })
-    .then(
-      tokenRefreshResponse => {
-      localStorage.setItem('token', tokenRefreshResponse.data.payload.token);
-      localStorage.setItem('refresh_token', tokenRefreshResponse.data.payload.refresh_token);
-      failedRequest.response.config.headers['Authorization'] = 'Bearer ' + tokenRefreshResponse.data.payload.token;
-      
-      return Promise.resolve();
-    })
-    .catch(error => {
-      Promise.reject(error);
-      toast.error('Something went wrong. Please log in again.');
+const refreshAuth = async (failedRequest) => {
+  if (localStorage.getItem('refresh_token') == null || localStorage.getItem('refresh_token') === '') {
+    toast.error('Refresh token is missing, Log in again.');
 
-      localStorage.removeItem('token');
-      localStorage.removeItem('refresh_token');
-      localStorage.removeItem('user_data');
-      localStorage.removeItem('geo_data');
+    localStorage.removeItem('token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem('user_data');
 
-      return window.location.replace('/auth/sign-in');
-    });
+    return window.location.replace('/auth/sign-in');
+  }
 
-createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic);
+  await axiosInstance.get(`/auth/refresh`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('refresh_token')}`,
+      'X-Request-ID': uid,
+      'Accept': 'application/json'
+    }
+  })
+  .then(
+    tokenRefreshResponse => {
+    localStorage.setItem('token', tokenRefreshResponse.data.payload.token);
+    localStorage.setItem('refresh_token', tokenRefreshResponse.data.payload.refresh_token);
+    failedRequest.response.config.headers['Authorization'] = 'Bearer ' + tokenRefreshResponse.data.payload.token;
+
+    Promise.resolve(tokenRefreshResponse.data.payload.token);
+    window.location.reload(false);
+  })
+  .catch(error => {
+    toast.error('Something went wrong. Please log in again.');
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('refresh_token');
+    localStorage.removeItem('user_data');
+
+    return window.location.replace('/auth/sign-in');
+  });
+}
+
+createAuthRefreshInterceptor(axiosInstance, refreshAuth);
 
 const fetcher = (method, url, data) => {
   let headers = { };
 
   if (localStorage.getItem('token')) {
     headers = {
-      Authorization: `Bearer ${localStorage.getItem('token')}`
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'X-Request-ID': uid,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     };
   }
+
   return axiosInstance({
     method,
     url,
